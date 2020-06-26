3c64d3cf9cf9a83dfaac9b0da56dd16272005bff
Oriane <oriane.martinet@gmail.com>
2020-06-26 14:19:14 +0200

    fix: fix error not display array

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ $(document).ready( function () {
                 } else {
                     return data;
                 }
+            }
             }],
         searchPanes: {
             panes: [
